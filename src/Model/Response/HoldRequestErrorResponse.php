33d1e1b6c5d35e06215e31c41b734e32919fd18c
Gregory Kallenberg <gregorykallenberg@nypl.org>
2017-07-10 14:25:18 -0400

    update error response; add starter dir to swagger generate method

--- src/Model/Response/HoldRequestErrorResponse.php
+++ src/Model/Response/HoldRequestErrorResponse.php
@@ -8,16 +8,4 @@ use NYPL\Starter\Model\Response\ErrorResponse;
  */
 class HoldRequestErrorResponse extends ErrorResponse
 {
-    /**
-     * HoldRequestErrorResponse constructor.
-     *
-     * @param int    $statusCode
-     * @param string $type
-     * @param string $message
-     * @param null   $exception
-     */
-    public function __construct($statusCode = 500, $type = '', $message = '', $exception = null)
-    {
-        parent::__construct($statusCode, $type, $message, $exception);
-    }
 }
